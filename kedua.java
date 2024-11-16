4a3b4401305f64aa0ea4e8293b25b97f131c841c
EYELKUYEL <rayalfarizy22@gmail.com>
2024-11-16 13:22:50 +0700

    Add files via upload

---
+++ kedua.java
@@ -0,0 +1,13 @@
+ public class kedua {
+    public static void main(String[] args) {
+
+        String namaawal = "Kurey ";
+        String namaakhir = "Al Farizy";
+
+        // Menampilkan panjang string ke1
+        System.out.println(namaawal);
+
+        // Menampilkan panjang string ke2
+        System.out.println(namaakhir);
+    }
+}
