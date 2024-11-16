67e2992b11280589e8af030a252e2521acdc19de
EYELKUYEL <rayalfarizy22@gmail.com>
2024-11-16 13:19:31 +0700

    Add files via upload

---
+++ kesatu.java
@@ -0,0 +1,13 @@
+ public class kesatu {
+    public static void main(String[] args) {
+
+        String ke1 = "apa we";
+        String ke2 = "Sehat Kau";
+
+        // Menampilkan panjang string ke1
+        System.out.println("Panjang string ke1: " + ke1.length());
+
+        // Menampilkan panjang string ke2
+        System.out.println("Panjang string ke2: " + ke2.length());
+    }
+}
